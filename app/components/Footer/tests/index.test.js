b51ef4091486ce099fbd0681f134b81d0cb096c5
Edyta <edyta.s@amusenetwork.com>
2019-12-04 14:17:27 +0100

    cleaning code

--- app/components/Footer/tests/index.test.js
+++ app/components/Footer/tests/index.test.js
@@ -4,17 +4,17 @@ import { shallow } from 'enzyme';
 import Footer from '../index';
 
 describe('<Footer />', () => {
-  it('should render the copyright notice', () => {
+  it('should render the footer notice', () => {
     const renderedComponent = shallow(<Footer />);
     expect(
       renderedComponent.contains(
-        <section>This project is licensed under the MIT license.</section>
+        <section>Footer</section>
       )
     ).toBe(true);
   });
 
   it('should render the credits', () => {
     const renderedComponent = shallow(<Footer />);
-    expect(renderedComponent.text()).toContain('Dinesh Pandiyan');
+    expect(renderedComponent.text()).toContain('Credits');
   });
 });
--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -4,13 +4,14 @@
  * This is the first thing users see of our App, at the '/' route
  */
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
-import ReposList from 'components/ReposList';
-import './style.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+import ReposList from "components/ReposList";
+import "./style.scss";
 
-export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+export default class HomePage extends React.PureComponent {
+  // eslint-disable-line react/prefer-stateless-function
   /**
    * when initial state username is not null, submit the form to load repos
    */
@@ -23,7 +24,12 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
 
   render() {
     const {
-      loading, error, repos, username, onChangeUsername, onSubmitForm
+      loading,
+      error,
+      repos,
+      username,
+      onChangeUsername,
+      onSubmitForm
     } = this.props;
     const reposListProps = {
       loading,
@@ -35,15 +41,16 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
       <article>
         <Helmet>
           <title>Home Page</title>
-          <meta name="description" content="A React.js Boilerplate application homepage" />
+          <meta
+            name="description"
+            content="A React.js Boilerplate application homepage"
+          />
         </Helmet>
         <div className="home-page">
-        
           <section>
             <form onSubmit={onSubmitForm}>
               <label htmlFor="username">
-                Show Github repositories by
-                <span className="at-prefix">@</span>
+                Form <span className="at-prefix">@</span>
                 <input
                   id="username"
                   type="text"
